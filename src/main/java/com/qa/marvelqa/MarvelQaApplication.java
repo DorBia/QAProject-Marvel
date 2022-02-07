9edbce211b8edeef83365dd2e5bbf3a004fbcaf2
DorBia <dorota.bialy95@gmail.com>
2022-02-07 12:06:35 +0000

    Initial commit

---
+++ src/main/java/com/qa/marvelqa/MarvelQaApplication.java
@@ -0,0 +1,13 @@
+package com.qa.marvelqa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MarvelQaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MarvelQaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/qa/marvelqa/MarvelQaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.qa.marvelqa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MarvelQaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
