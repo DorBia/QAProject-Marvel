4fd6bd7c41b587b7886d0b8bc96103a1902eff12
DorBia <dorota.bialy95@gmail.com>
2022-02-07 12:22:49 +0000

    Added the getters and setters to the domain

--- src/main/java/com/qa/marvelqa/domain/MarvelCharacter.java
+++ src/main/java/com/qa/marvelqa/domain/MarvelCharacter.java
@@ -4,6 +4,7 @@ import javax.persistence.*;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Past;
 import java.time.LocalDate;
+import java.time.Period;
 
 @Entity
 public class MarvelCharacter {
@@ -65,5 +66,63 @@ public class MarvelCharacter {
         this.actorsName = actorsName;
         this.actorsDOB = actorsDOB;
         this.age = age;
+    }
+
+    //Getters and setters
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getCharactersName() {
+        return charactersName;
+    }
+
+    public void setCharactersName(String fullName) {
+        this.charactersName = fullName;
+    }
+
+    public String getAlias() {
+        return alias;
+    }
+
+    public void setAlias(String alias) {
+        this.alias = alias;
+    }
+
+    public String getMainAbility() {
+        return mainAbility;
+    }
+
+    public void setMainAbility(String mainAbility) {
+        this.mainAbility = mainAbility;
+    }
+
+    public String getActorsName() {
+        return actorsName;
+    }
+
+    public void setActorsName(String actorsName) {
+        this.actorsName = actorsName;
+    }
+
+    public LocalDate getActorsDOB() {
+        return actorsDOB;
+    }
+
+    public void setActorsDOB(LocalDate actorsDOB) {
+        this.actorsDOB = actorsDOB;
+    }
+
+    public int getAge() {
+        return Period.between(this.actorsDOB, LocalDate.now()).getYears();
+    }
+
+    public void setAge(int years) {
+        this.age = Period.between(this.actorsDOB, LocalDate.now()).getYears();
     }
 }
